cd9f3c2870cea0c06ffcea51e1c9dfc4fa1424a6
ichoi21 <ihchoi@gmail.com>
2020-04-25 20:36:52 -0700

    timer function added

--- app.js
+++ app.js
@@ -2,16 +2,26 @@
 $(document).ready(function () {
   var score = 0;
   var questionIndex;
-  var secondsLeft = 30;
+  var secondsLeft = 45;
   var timeInterval;
 
   //need to add JSON parse to use LocalStorage for Scores
 
   // create a function to refresh the html on the screen each time the question is changed
-  function refreshQuestions() {}
+  function refreshQ() {}
 
   // Need to create a timer for quiz
-  function setSpeed() {}
+  function countdown() {
+    var timer = document.querySelector("#timeclock");
+    timerInterval = setInterval(function () {
+      secondsLeft--;
+      timer.textContent = "0:" + secondsLeft + " seconds left!";
+      if (secondsLeft < 1) {
+        clearInterval(timerInterval);
+        sendMessage("Guess You're Not Current");
+      }
+    }, 1000);
+  }
 
   // Need to use 4/23 class for todo list to prepend recorded scores
   function showHighscores() {}
