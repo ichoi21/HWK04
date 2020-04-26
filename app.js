b5344feb36711ef57b3df93a0a5e896b0c287e61
ichoi21 <ihchoi@gmail.com>
2020-04-26 00:04:16 -0700

    fixed #btnScore to read LocalStorage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var score = 0;
 var questionIndex;
 var secondsLeft = 45;
-var timeInterval;
+var timerInterval;
 
 //need to add JSON parse to use LocalStorage for Scores
 try {
@@ -30,17 +30,60 @@ function refreshQ() {
   }
 }
 
-// timer for quiz
+// timer for quiz (counter subtracting)
 function countdown() {
   var timer = document.querySelector("#timeclock");
   timerInterval = setInterval(function () {
     secondsLeft--;
-    timer.textContent = "0:" + secondsLeft + " seconds left!";
+    timer.textContent = "You have " + secondsLeft + " seconds left!";
     if (secondsLeft < 1) {
       clearInterval(timerInterval);
       sendMessage("Guess You're Not Current");
     }
   }, 1000);
+}
+
+//After Quiz is done, need a command to record score and generate score list
+function showScores() {
+  $("#timeclock").html("");
+  $("#header").html("");
+  $("#body").attr("class", "col-md-12 text-center");
+  $("#body").html(`
+    <table id="scoreTable" class="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Rank</th>
+          <th scope="col">Alias</th>
+          <th scope="col">Points</th>
+        </tr>
+      </thead>
+      <tbody id="tbody"></tbody>
+    </table>
+    
+    <a href="./index.html" class="btn btn-outline-success m-2">
+      Return
+    </a>
+    <button id="reset" class="btn btn-outline-danger m-2">Reset</button>
+    `);
+
+  clearInterval(timerInterval);
+  rankArray.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  if (rankArray.length < 10) {
+    rankLength = rankArray.length;
+  } else {
+    rankLength = 10;
+  }
+
+  for (var i = 0; i < rankLength; i++) {
+    var currentRank = i + 1;
+    $("#tbody").append(`<tr id="row-${i}"></tr>`);
+    $(`#row-${i}`).append(`<td>${currentRank}</td>`);
+    $(`#row-${i}`).append(`<td>${rankArray[i].name}</td>`);
+    $(`#row-${i}`).append(`<td>${rankArray[i].score}</td>`);
+  }
 }
 
 // function to start the quiz and to show Scores
@@ -79,50 +122,8 @@ $(document).on("click", "#btnAnswer", function () {
   }
 });
 
-//After Quiz is done, need a command to record score and generate score list
-function showScores() {
-  $("#timeclock").html("");
-  $("#header").html("");
-  $("#body").attr("class", "col-md-12 text-center");
-  $("#body").html(`
-    <table id="scoreTable" class="table table-striped table-dark">
-      <thead>
-        <tr>
-          <th scope="col">Rank</th>
-          <th scope="col">Alias</th>
-          <th scope="col">Points</th>
-        </tr>
-      </thead>
-      <tbody id="tbody"></tbody>
-    </table>
-    
-    <a href="./index.html" class="btn btn-outline-success m-2">
-      Return
-    </a>
-    <button id="reset" class="btn btn-outline-danger m-2">Reset</button>`);
-
-  clearInterval(timerInterval);
-  rankArray.sort(function (a, b) {
-    return b.score - a.score;
-  });
-
-  if (rankArray.length < 10) {
-    rankLength = rankArray.length;
-  } else {
-    rankLength = 10;
-  }
-
-  for (var i = 0; i < rankLength; i++) {
-    var currentRank = i + 1;
-    $("#tbody").append(`<tr id="row-${i}"></tr>`);
-    $(`#row-${i}`).append(`<td>${currentRank}</td>`);
-    $(`#row-${i}`).append(`<td>${rankArray[i].name}</td>`);
-    $(`#row-${i}`).append(`<td>${rankArray[i].score}</td>`);
-  }
-}
-
 function sendMessage(str) {
-  $("#header").html(`<h2 class="text-center col-md-12">${str}</h2>`);
+  $("#timeclock").html("You finished with " + secondsLeft + " seconds left!");
   $("#body").attr("class", "col-md-12 text-center");
   $("#body").html(
     `<h2>You scored ${score} points!</h2>
@@ -132,12 +133,11 @@ function sendMessage(str) {
           type="text"
           name="alias"
           id="alias"
-          autofocus
-          placeholder="Enter an Alias"/>
+          autofocus placeholder="Enter an Alias"/>
         <button id="btnSubmit" class="btn btn-outline-primary">Save</button>
       </form>
       <br />
-      <form><button id="scoreBtn" class="btn btn-outline-success">Don't Record</button>
+      <form><button id="btnScore" class="btn btn-outline-success">Don't Record</button>
       <button type="submit" class="btn btn-outline-danger">Try Again</button></form>`
   );
 }
@@ -148,7 +148,7 @@ function showAlert(str, type) {
   $("#alert").text(str);
   window.setTimeout(function () {
     $("#alert").hide();
-  }, 1000);
+  }, 3000);
 }
 
 $(document).on("click", "#btnSubmit", function (e) {
@@ -166,7 +166,7 @@ $(document).on("click", "#btnSubmit", function (e) {
   }
 });
 
-// bonus to reset Scores (clear LocalStorage) #reset
+// Reset Ranks/Scores Array (clear LocalStorage) #reset
 $(document).on("click", "#reset", function () {
   window.localStorage.clear();
   $("").html("");
