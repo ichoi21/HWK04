f0b729237a2cc5eca90634a55eb1724e538cdc14
ichoi21 <ihchoi@gmail.com>
2020-05-14 20:04:14 -0700

    first commit of new quiz

---
+++ Assets/questions.js
@@ -0,0 +1,52 @@
+const Questions = [
+  [
+    "Q1 of 6: What is your coffee experience level?",
+    "I'm pretty new to all of this",
+    "I’m at an intermediate stage",
+    "I’m pretty advanced",
+    "I’m a total coffee nerd",
+  ],
+  [
+    "Q2 of 6: What's your preferred way to making coffee?",
+    "Coffee Maker",
+    "French Press/Pour Over",
+    "Cold Brew",
+    "Nespresso",
+  ],
+  [
+    "Q3 of 6: Do you add anything to your coffee?",
+    "Milk and/or Cream",
+    "Sugar, Sweetener and/or Syrups",
+    "Straight up Black",
+    "A mix of these choices",
+  ],
+  [
+    "Q4 of 6: Do you have a prefered roast level?",
+    "Light Roast",
+    "Medium Roast",
+    "Dark Roast",
+    "Not picky (or I'm not sure)",
+  ],
+  [
+    "Q5 of 6 : How do you like your coffee to taste",
+    "Classic and traditional",
+    "Hints of something different",
+    "Surprising and unconventional",
+    "I'm not quite sure what I like",
+  ],
+  [
+    "Q6 of 6 : How much do you spend on a cup of coffee regularly?",
+    "$$",
+    "$$$",
+    "$$$$",
+    "$$$$$",
+  ],
+];
+const Answers = [
+  "I'm a total coffee nerd",
+  "Cold Brew",
+  "Straight Up Black",
+  "Light Roast",
+  "Classic and traditional",
+  "$$$",
+];
